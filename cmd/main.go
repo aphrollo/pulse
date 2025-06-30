abf6fb860a61e410683bf902abac2cad484f6222
sprayKING <trickstertwo@gmail.com>
2025-06-30 20:09:11 +0200

    improvement: htmx as local static js file

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/aphrollo/pulse/internal/handlers"
 	"github.com/gofiber/fiber/v2"
 )
@@ -8,9 +10,27 @@ import (
 func main() {
 	app := fiber.New()
 
+	// Static resources; images, js, css
+	app.Static("/", "./static", fiber.Static{
+		Compress:      true,
+		ByteRange:     true,
+		Browse:        false,
+		Download:      false,
+		CacheDuration: 5 * time.Minute,
+		MaxAge:        86400,
+		ModifyResponse: func(c *fiber.Ctx) error {
+			c.Set("Cache-Control", "public, max-age=86400")
+			return nil
+		},
+	})
+
 	app.Get("/", handlers.DashboardHandler)
 
-	app.Static("/static", "./static") // For any static HTMX/js/css
+	// Favicon
+	//app.Use(favicon.New(favicon.Config{
+	//	File: "./static/images/favicon.ico",
+	//	URL:  "/favicon.ico",
+	//}))
 
 	if err := app.Listen(":3000"); err != nil {
 		panic(err)
